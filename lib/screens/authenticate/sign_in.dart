ff7c6506f3bdd45bd63152749d4b26da447933f2
karimax2 <lazaarworks@gmail.com>
2020-04-16 22:34:41 +0100

    Register and sign in with email

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,6 +2,10 @@ import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
+
+  final Function toggleView;
+  SignIn({ this.toggleView});
+
   @override
   _SignInState createState() => _SignInState();
 }
@@ -9,6 +13,12 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
 
   final AuthService _auth = AuthService();
+  final _formKey =GlobalKey<FormState>();
+
+  // text field state
+  String email = '';
+  String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -18,22 +28,58 @@ class _SignInState extends State<SignIn> {
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
         title: Text('Sign in to Brew Crew'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Register'),
+            onPressed: () {
+              widget.toggleView();
+            }
+           )
+        ],
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.00),
-        child: RaisedButton(
-          child: Text('Sign in Anonymously'), 
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print('error signing in');
-            }else {
-              print('signed in');
-              print(result.uid);
-            }
-        },
-        ),
-      ),
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.00),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val) {
+                  setState(() => email = val);
+                }),
+                TextFormField(
+                    validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    }),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  color: Colors.pink[400],
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()){
+                      dynamic result = await _auth.signInWithEmailAndPaswword(email, password);
+                      if(result == null) {
+                        setState(() => error = 'Could not sign in');
+                      }
+                    }
+                  },
+                ),
+                SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              ),
+              ],
+            ),
+          )),
     );
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -32,9 +32,30 @@ Future signInAnon() async {
 }
 
 // sign in with email & password 
+Future signInWithEmailAndPaswword(String email, String password) async {
+  try {
+    AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+    FirebaseUser user = result.user;
+    return _userFromFirebaseUser(user);
+  } catch(e){
+    print(e.toString());
+    return null;
+  }
+}
+
+
 
 // register with email & password
-
+Future registerWithEmailAndPaswword(String email, String password) async {
+  try {
+    AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    FirebaseUser user = result.user;
+    return _userFromFirebaseUser(user);
+  } catch(e){
+    print(e.toString());
+    return null;
+  }
+}
 
 // sign out
 Future signOut() async {
